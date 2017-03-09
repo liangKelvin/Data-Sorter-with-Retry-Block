1ae3c5772311c268a6ee653b49aad3e1623600c4
Kelvin Liang <kelvinliang@Kelvins-MacBook-Pro.local>
2017-03-09 13:20:04 -0700

    lol

--- Adjudicator.java
+++ Adjudicator.java
@@ -20,13 +20,25 @@ public class Adjudicator {
 			System.out.println("Error output array and input array not the same size");
 			return false;
 		}
+
 		// is sorting correct?
+		int sumIn = 0;
+		int sumOut = 0;
 		for(int i = 0; i < outputValues.size()-1; i++) {
+			sumIn += inputValues.get(i);
+			sumOut += outputValues.get(i);
 			if(outputValues.get(i) > outputValues.get(i+1)) {
 				System.out.println("Sorting incorrect");
 				return false; 
 			}
 		}
+		// // sumcheck
+		// sumIn += inputValues.get(inputValues.size()-1);
+		// sumOut += inputValues.get(outputValues.size()-1);
+		// if(sumOut != sumIn) {
+		// 	System.out.println("Sum Check incorrect");
+		// 	return false;
+		// }
 		return true;
 	}
 }
